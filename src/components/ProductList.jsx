be1990b637a6b87cd6619cf01d3609e6e56fdcab
vnatale52 <vnatale52@gmail.com>
2025-10-19 13:27:58 -0300

    Add files via upload

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,7 +5,7 @@ import MiniCartButton from './MiniCartButton.jsx'
 import CartPanel from './CartPanel.jsx' // Importa CartPanel para su estado
 import { useCart } from '../context/CartContext.jsx'
 
-const DEFAULT_LOCAL = '/src/services/demo-products.json'
+const DEFAULT_LOCAL = '/dist/demo-products.json'   // probado también con '/src/services/demo-products.json' y en /public
 
 async function fetchFromMock(url){
   const res = await fetch(url)
